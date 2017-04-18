87ed69a6143fe26d4c827c0065a8b9b5a2b46c1a
Danil Ivanov <nonamenix@gmail.com>
2017-04-18 08:22:00 +0300

    * Moderator commands that allowed only for moderators * Let me search for you in google and wiki

---
+++ bot/bot.py
@@ -0,0 +1,60 @@
+from typing import List
+
+from aiotg import Bot as BaseBot, API_TIMEOUT, Chat
+import re
+
+
+class Bot(BaseBot):
+    def __init__(self, api_token: str, moderators: List[int],
+                 api_timeout: int = API_TIMEOUT,
+                 botan_token=None,
+                 name=None):
+        super(Bot, self).__init__(
+            api_token=api_token,
+            api_timeout=api_timeout,
+            botan_token=botan_token,
+            name=name
+        )
+
+        self.moderators = moderators
+        self._moderators_commands = []
+
+    def add_moderators_command(self, regexp, fn):
+        """
+        Manually register regexp based command
+        """
+        self._moderators_commands.append((regexp, fn))
+
+    def moderator_command(self, regexp):
+        """
+        Register a new command that allowed only for moderators
+
+        :param str regexp: Regular expression matching the command to register
+
+        :Example:
+
+        >>> @bot.moderator_command(r"/echo (.+)")
+        >>> def echo(chat, match):
+        >>>     return chat.reply(match.group(1))
+        """
+
+        def decorator(fn):
+            self.add_moderators_command(regexp, fn)
+            return fn
+
+        return decorator
+
+    def _process_message(self, message):
+
+        chat = Chat.from_message(self, message)
+
+        if message['from']['id'] in self.moderators:
+            for patterns, handler in self._moderators_commands:
+                m = re.search(patterns, message["text"], re.I)
+                if m:
+                    self.track(message, handler.__name__)
+                    return handler(chat, m)
+
+        handler = super(Bot, self)._process_message(message)
+
+        return handler
--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,9 @@
 import os
 
-from aiotg import Bot, Chat, aiohttp
+from urllib.parse import quote
+
+from aiotg import Chat, aiohttp
+from bot import Bot
 import logging
 
 # Logging
@@ -12,58 +15,56 @@ logger = logging.getLogger(__name__)
 ch = logging.StreamHandler()
 logger.addHandler(ch)
 
-show_pep_info = os.environ.get("SHOW_PEP_INFO", True)
+DEBUG = "DEBUG" in os.environ
+SHOW_PEP_INFO = "SHOW_PEP_INFO" in os.environ
+
+
+def get_moderators():
+    moderators = [
+        132982472,  # nonamenix
+        59323058  # lig11
+    ]
+    try:
+        moderators = os.environ['MODERATORS']
+    except IndexError:
+        pass
+    else:
+        moderators = [int(m.strip()) for m in moderators.split(' ')]
+    logger.info(moderators)
+    return moderators
+
 
 bot = Bot(
     api_token=os.environ["BOT_TOKEN"],
+    moderators=get_moderators()
 )
 
 
 def send_code(chat: Chat, text: str):
     """Wrap text to code snippet and send as answer"""
+
     return chat.reply("```{}```".format(text), parse_mode="Markdown")
 
 
-if show_pep_info:
-    pep_link = "https://www.python.org/dev/peps/pep-{0:04d}/"
-
-
-    async def is_pep_exists(pep):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(pep_link.format(pep)) as resp:
-                return resp.status == 200
-
-
-    @bot.command(".*pep-?(?P<pep>\d{1,4})")
-    async def peps(chat: Chat, matched):
-        try:
-            pep = int(matched.group('pep'))
-        except ValueError:
-            pass
-        else:
-            if await is_pep_exists(pep):
-                await chat.send_text(pep_link.format(pep), reply_to_message_id=chat.message["message_id"])
-
-
-@bot.command("/?ping")
+@bot.moderator_command("/?ping")
 async def ping(chat, message):
     await chat.reply("pong")
 
 
-@bot.command("/?import __hello__")
+@bot.moderator_command("/?import __hello__")
 async def hello(chat: Chat, message):
     await send_code(chat, " Hello world")
 
 
-@bot.command("/?import this")
+@bot.moderator_command("/?import this")
 async def zen(chat: Chat, message):
     # TODO: fetch it from chat_zen_url = "https://raw.githubusercontent.com/spbpython/orgs-wiki/master/chat/this.md"
-    await chat.send_chat_action('typing')
-    await send_code(chat, """
-# The Zen of SPb Python Chat
-*(Inspired by "The Zen of Python, by Tim Peters")*
 
-- Short inroduction of yourself is better than "hello".
+    await chat.reply("""
+*The Zen of SPb Python Chat*
+_(Inspired by "The Zen of Python, by Tim Peters")_
+
+- Short introduction of yourself is better than "hello".
 - Link to gist is better than source paste.
 - One long message is better than many short.
 - Editing the message is better than correcting via another one.
@@ -75,10 +76,57 @@ async def zen(chat: Chat, message):
 - Don't ask to ask just ask.
 - Text message is better than voice message.
 - Unless it is voice conference.
-- Git repos are one honking great idea -- let's do more of those!
-""")
+- Git repos are one honking great idea — let's do more of those!
+""", parse_mode="Markdown")
 
+
+def reply_with_let_me_search_for_you(chat: Chat, search_url: str, query: str):
+    return chat.send_text(
+        search_url.format(query=quote(query)),
+        reply_to_message_id=chat.message["message_id"]
+    )
+
+
+@bot.moderator_command("/g (?P<query>.+)")
+@bot.moderator_command("/google (?P<query>.+)")
+async def google(chat: Chat, matched):
+    await reply_with_let_me_search_for_you(
+        chat,
+        search_url="https://www.google.ru/search?q={query}",
+        query=matched.group('query')
+    )
+
+
+@bot.moderator_command("/w (?P<query>.+)")
+@bot.moderator_command("/wiki (?P<query>.+)")
+async def wiki(chat: Chat, matched):
+    await reply_with_let_me_search_for_you(
+        chat,
+        search_url="https://en.wikipedia.org/w/index.php?search={query}",
+        query=matched.group('query')
+    )
+
+
+if SHOW_PEP_INFO:
+    pep_link = "https://www.python.org/dev/peps/pep-{0:04d}/"
+
+
+    async def is_pep_exists(pep):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(pep_link.format(pep)) as resp:
+                return resp.status == 200
+
+
+    @bot.moderator_command(".*pep-?(?P<pep>\d{1,4})")
+    async def peps(chat: Chat, matched):
+        try:
+            pep = int(matched.group('pep'))
+        except ValueError:
+            pass
+        else:
+            if await is_pep_exists(pep):
+                await chat.send_text(pep_link.format(pep), reply_to_message_id=chat.message["message_id"])
 
 if __name__ == "__main__":
     logger.info("Running...")
-    bot.run()
+    bot.run(debug=DEBUG)
